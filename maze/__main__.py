8d1e3345869beb5f5748d9bd61797c3d904a2590
EnriqueBenny <erikben@byui.edu>
2022-03-18 01:27:23 -0600

    notes

--- maze/__main__.py
+++ maze/__main__.py
@@ -2,3 +2,9 @@ from maze.game.directing import Director
 
 def main():
     pass
+
+
+"""Rules: player moves though the maze to the goal.
+If the player touches a wall, the maze is reset, and the player loses a life.
+
+Code from cycle and greed has been recycled."""
