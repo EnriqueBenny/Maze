d650a3d4fc7042b0afa67ae383e92f3acb3633eb
EnriqueBenny <erikben@byui.edu>
2022-03-24 15:16:58 -0600

    maze created in constants

--- maze/game/constants.py
+++ maze/game/constants.py
@@ -14,3 +14,28 @@ WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
 YELLOW = Color(255, 255, 0)
 GREEN = Color(0, 255, 0)
+
+# maze (w is a wall, s is a space, G is goal):
+"""
+s s s s s w s s s w s s s s s s s s s w w s s s s w s s s s s s s s s s s s s s
+w s w w s w s w s w s w w s w w w w s s s s w w s w w w w w w s w s w s w w w w
+w s s s w w s w s s s w w s s s w w s w w w w w w w s s s s s s w s w s s s s s
+w w w s s s s w w w w w w s w s w w s s s s s s w w s w w w w w w s w w w w s w
+w s s s w w w w w s s s w s w s s w s w w w w s w w s w s s s s s s w s s s s w
+s s w w w s s s s s w w w s w w s s s s s w s s s s s w s w w w w w s s w w w w
+w s s w w w w w w s w w w s w w s w w w s w s w w w w w s w w s s s w s w w w s
+w w s w s s s s w s s s w s w w s w w w s w s w s s s w s w s s w s w s s s s s
+s s s w s w w s w w w s w s w w s w w w s w s w w s w w s s s w w s s w w w w s
+s w s w s w w s s s s s w s w w s s s s s s s w w s s w w w w s s w s s s s w w
+s w w w s s w w w w w w w s w w w w w s w w w w w w s w w w w w s w w w w s s s
+s s w w w s s s w s s s s s s s s s w s s s s s w w s s s s s s s s s s w w w s
+w s s w w s w s w w w w w s w w w s w w w s w s s s s w w w w w w w w w w w w s
+w w w s w s w s w s s s w s w w w s w w w s w w s w s w s s s s s s s s s s s s
+s s w s w s w s s s w s w s s s s s s s w s w w s w s w s w w w w w w s w w w w
+w s w s w s w s w w w s w w w w w w w s s s s w s w s w s w s s s s w s s s s w
+s s s s w s w s w w w s w s s s s s w s w w s w s w s w s s s w w s w w w w s s
+s w w w w s w s w w s s w s w w w s w s w w s w s w s w w w w w w s w w w w w s
+s s s s w s w s s w w s s s s w w s w s w w s s s s s s s s s s w s s s s s w s
+w s w s w w w s w w w s w w s w w s w s w w w w w w w w w w w s w w w w w s s w
+w s w s s s s s s s s s s w s s s s w s s s s s s s s s s s s s s s s s w w s G
+"""
