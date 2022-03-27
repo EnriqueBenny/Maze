597842c2b63608950055847dc2bf62f0ecce0f75
Kelson-G <you@example.com>
2022-03-26 18:42:39 -0600

    small changes to keyboard service

--- maze/game/services/keyboard_service.py
+++ maze/game/services/keyboard_service.py
@@ -20,16 +20,11 @@ class KeyboardService:
         self._keys['s'] = pyray.KEY_S
         self._keys['d'] = pyray.KEY_D
 
-        self._keys['i'] = pyray.KEY_I
-        self._keys['j'] = pyray.KEY_J
-        self._keys['k'] = pyray.KEY_K
-        self._keys['l'] = pyray.KEY_L
-
     def is_key_up(self, key):
         """Checks if the given key is currently up.
         
         Args:
-            key (string): The given key (w, a, s, d or i, j, k, l)
+            key (string): The given key (w, a, s, d)
         """
         pyray_key = self._keys[key.lower()]
         return pyray.is_key_up(pyray_key)
@@ -38,7 +33,7 @@ class KeyboardService:
         """Checks if the given key is currently down.
         
         Args:
-            key (string): The given key (w, a, s, d or i, j, k, l)
+            key (string): The given key (w, a, s, d)
         """
         pyray_key = self._keys[key.lower()]
         return pyray.is_key_down(pyray_key)
